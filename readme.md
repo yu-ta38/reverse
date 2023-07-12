7b19c6e1823ecff8fd328c37ece3dbdbc950deb7
Ryo Takaishi <m230744@hiroshima-u.ac.jp>
2023-07-12 02:47:05 +0000

    can reverse

--- readme.md
+++ readme.md
@@ -2,4 +2,8 @@ first push
 
 command list
 
-go 
+go run <>.go
+
+他のリポジトリで分担作業する場合
+git clone <>.git
+git checkout develop
--- reverse.go
+++ reverse.go
@@ -15,6 +15,10 @@ func (b *Board) init() {
 			b.tokens[i][j] = 0
 		}
 	}
+	b.tokens[3][3] = 1
+	b.tokens[3][4] = 2
+	b.tokens[4][3] = 2
+	b.tokens[4][4] = 1
 }
 
 func (b *Board) display() {
@@ -32,8 +36,73 @@ func (b *Board) display() {
 	}
 }
 
+func (b *Board) put(x, y int, u string) {
+	if u == "O" {
+		b.tokens[x][y] = 1
+	} else if u == "X" {
+		b.tokens[x][y] = 2
+	}
+}
+
+//func (b *Board) get(x, y int) string {
+//	get_num := ""
+//	if b.tokens[x][y] == 0 {
+//		get_num = "."
+//	} else if b.tokens[x][y] == 1 {
+//		get_num = "O"
+//	} else if b.tokens[x][y] == 2 {
+//		get_num = "X"
+//	}
+//	return get_num
+//}
+
+func (b *Board) reverse(x, y int, u string) {
+	// 探索の方向
+	dx := [](int){1, 0, -1, 0, 1, 1, -1, -1}
+	dy := [](int){0, 1, 0, -1, 1, -1, 1, -1}
+	var player, enemy int
+
+	if u == "O" {
+		player = 1
+		enemy = 2
+	} else if u == "X" {
+		player = 2
+		enemy = 1
+	}
+
+	for i := 0; i < GRID; i++ {
+		ix := x + dx[i]
+		iy := y + dy[i]
+		for {
+			if ix < 0 || ix >= GRID || iy < 0 || iy >= GRID {
+				break
+			}
+			if b.tokens[ix][iy] == 0 {
+				break
+			} else if b.tokens[ix][iy] == player {
+				break
+			} else if b.tokens[ix][iy] == enemy {
+				ix += dx[i]
+				iy += dy[i]
+			}
+		}
+		for !(ix == x && iy == y) {
+			ix -= dx[i]
+			iy -= dy[i]
+			b.tokens[ix][iy] = player
+			// 			ix -= dx[i]
+			// 			iy -= dy[i]
+		}
+	}
+}
+
 func main() {
 	var board Board
 	board.init()
+
+	board.put(3, 5, "O")
+	board.reverse(3, 5, "O")
+	//	board.put(1, 1, "X")
+
 	board.display()
 }
